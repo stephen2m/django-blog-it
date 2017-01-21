ebee044d1d0e2ad7ab210f379b77e85ac5e002ec
Stephen Mutua <stephenmue@gmail.com>
2017-01-22 02:31:44 +0300

    start on tweaks to remove character escaping

--- django_blog_it/django_blog_it/templatetags/highlight_code.py
+++ django_blog_it/django_blog_it/templatetags/highlight_code.py
@@ -1,4 +1,6 @@
 import re
+import htmlentitydefs
+from bs4 import BeautifulSoup
 from django import template
 from pygments import highlight
 from pygments.lexers import guess_lexer
@@ -7,6 +9,34 @@ from pygments.formatters.html import HtmlFormatter
 
 register = template.Library()
 code_regex = re.compile(r'<pre>(.*?)</pre>', re.DOTALL)
+
+
+def unescape(text):
+    """
+    Removes HTML or XML character references and entities from a text string.
+
+    :param text The HTML (or XML) source text.
+    :return The plain text, as a Unicode string, if necessary.
+    """
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
 
 
 @register.filter
@@ -22,7 +52,9 @@ def pygmentify(value):
             except ValueError:
                 lexer = PythonLexer()
 
-            pygmented_string = highlight(code_string, lexer, HtmlFormatter())
+            pygmented_string = highlight(
+                code_string, lexer, HtmlFormatter()
+            )
             to_return += value[last_end:match_obj.start(1)] + \
                 pygmented_string
             last_end = match_obj.end(1)
